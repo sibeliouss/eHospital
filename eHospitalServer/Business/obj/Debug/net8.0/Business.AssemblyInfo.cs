7ef4a2d686b22a6b66c08f0f0ecff4b91e92389b
sibeliouss <sibelmozturk@hotmail.com>
2024-08-19 13:15:06 +0300

    init

--- eHospitalServer/Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
+++ eHospitalServer/Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4512afecbcd35e7cad24d75e3fdad9ed61df3bfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
